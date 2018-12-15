a32ae04a1a64241ca8473f3b6bbf42012f5435a3
phongdx62 <38428601+phongdx62@users.noreply.github.com>
2018-12-15 22:13:47 +0700

    fix

--- admin/admin.php
+++ admin/admin.php
@@ -1,6 +1,6 @@
 <?php  
 	session_start(); //Dòng lệnh session_start() sẽ đăng ký phiên làm việc của người dùng trên Server, từ đó Server sẽ tạo ra một ID riêng không trùng lặp để nhận diện cho client hiện tại. Dòng này bắt buộc có.
-	if($_SESSION["level"] == 1)
+	if($_SESSION["level"] == 2)
 	{		
 		require("templates/header.php");
 		require("templates/footer.php"); 
